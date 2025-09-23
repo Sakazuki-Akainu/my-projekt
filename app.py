adcadc09eab9fe107f20827e9f3900098e1969ed
fleet admiral <jujutsukaisen77700@gmail.com>
2025-09-23 19:26:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,8 @@ def upload_file():
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render gives the port
+    app.run(host="0.0.0.0", port=port, debug=False)
