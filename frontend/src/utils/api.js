9a945ddccca8da7268eadd682ee8d88b94c681d8
fleet admiral <jujutsukaisen77700@gmail.com>
2025-10-02 12:08:11 +0530

    Delete frontend directory

--- frontend/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const uploadFile = async (file) => {
-  const formData = new FormData();
-  formData.append('file', file);
-  const response = await axios.post('http://localhost:3000/upload', formData);
-  return response.data;
-};
